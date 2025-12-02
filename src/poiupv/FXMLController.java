41c6767c31f9b06cff87c8555945e63482b8f90d
LERI <LERI@10.236.63.173>
2025-12-02 12:17:36 +0100

    Prueba fxml

--- src/poiupv/FXMLController.java
+++ src/poiupv/FXMLController.java
@@ -18,9 +18,10 @@ public class FXMLController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    @Override
+
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
     
 }
